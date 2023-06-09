5f21a979aeef4e9b9e4ec7caa680f2ebfde5fe5e
Jose Luis Martinez <joseluismartinez1024@gmail.com>
2023-06-09 10:01:48 -0600

    Metodos principales
    Se crean los metodos principales en la interfaz

--- MicroserviciosRabbitMQ/MicroRabbit.Domain.Core/Bus/IEventBus.cs
+++ MicroserviciosRabbitMQ/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -8,5 +8,11 @@ namespace MicroRabbit.Domain.Core.Bus
 {
     public interface IEventBus
     {
+        // Crear mensaje en Queue y Consumir
+        void Publicar<T>(T evento) where T : Eventos;
+        void Leer<T, TH>() where T : Eventos where TH : IManejadorEventos<T>;
+
+        // Comunicacion interna entre clases
+        Task EnviarComando<T>(T Comando) where T : Comandos;
     }
 }
