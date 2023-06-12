778f0d82099d3c2989a616a96fb2e5ef4024e0b2
Jose Luis Martinez <joseluismartinez1024@gmail.com>
2023-06-12 17:00:17 -0600

    Crear proyecto de infraestructura
    Trabajo inicial en infra.bus

--- MicroserviciosRabbitMQ/MicroRabbit.Domain.Core/Eventos/Mensajes.cs
+++ MicroserviciosRabbitMQ/MicroRabbit.Domain.Core/Eventos/Mensajes.cs
@@ -2,13 +2,15 @@
 
 namespace MicroRabbit.Domain.Core.Eventos
 {
-    public abstract class Mensajes: IRequest<bool>
+    public abstract class Mensajes: IRequest<bool> // Se hereda para usar MediatR
     {
-        public string TipoMensaje { get; protected set; }
+        // Crear comunicacion entre 1 o mas componentes [patron mediator]
+        // IRequest permite comunicarse con otras clases
+        public string TipoMensaje { get; protected set; } // Mensaje que se enviara [Tipo]
 
         protected Mensajes()
         {
-            TipoMensaje = GetType().Name; // Extraer el nombre de clase que se ejecuta "Mensajes"
+            TipoMensaje = GetType().Name; // Extraer el nombre de clase que se ejecuta por ejemplo "Mensajes"
         }
     }
 }
---
+++ MicroserviciosRabbitMQ/MicroRabbit.Infra.Bus/RabbitMQBus.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Domain.Core.Comandos;
+using MicroRabbit.Domain.Core.Eventos;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace MicroRabbit.Infra.Bus
+{
+    // sealed: No permite heredar e implementa interfaz
+    public sealed class RabbitMQBus : IEventBus
+    {
+        private readonly IMediator _mediator; // Crear referencia del Mediator
+        // Diccionario para manejar eventos del Bus
+        private readonly Dictionary<string, List<Type>> _manejadores;
+        // Lista que almacena los tipos de eventos
+        private readonly List<Type> _tiposEvento;
+
+        public RabbitMQBus(IMediator mediator)
+        {
+            // Inyeccion de los objetos
+            _mediator = mediator;
+            _manejadores = new Dictionary<string, List<Type>>();
+            _tiposEvento = new List<Type>();
+        }
+
+        public Task EnviarComando<T>(T Comando) where T : Comandos
+        {
+            return _mediator.Send(Comando);
+        }
+
+        public void Leer<T, TH>()
+            where T : Eventos
+            where TH : IManejadorEventos<T>
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Publicar<T>(T evento) where T : Eventos
+        {
+            var Fabrica = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Password = "SAsa123$",
+                UserName = "sa",
+            };
+
+            using (var Conexion = Fabrica.CreateConnection())
+            using (var Canal = Conexion.CreateModel())
+            {
+                var NombreEvento = evento.GetType().Name; // Obtiene el nombre en tiempo de ejecucion
+                Canal.QueueDeclare(NombreEvento, false, false, false, null);
+                var Mensaje = JsonConvert.SerializeObject(evento);
+                var Cuerpo = Encoding.UTF8.GetBytes(Mensaje);
+                Canal.BasicPublish("", NombreEvento, null, Cuerpo);
+            }
+        }
+    }
+}
