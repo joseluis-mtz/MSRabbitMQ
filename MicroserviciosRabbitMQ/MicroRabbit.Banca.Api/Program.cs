b926722b9d7a212eb690582ade163ae02b7b9cfa
Jose Luis Martinez <joseluismartinez1024@gmail.com>
2023-06-23 18:22:54 -0600

    Configuraciones para RabbitMQ
    Crear la conexion e inyeccion de dependencias

--- MicroserviciosRabbitMQ/MicroRabbit.Banca.Api/Program.cs
+++ MicroserviciosRabbitMQ/MicroRabbit.Banca.Api/Program.cs
@@ -1,3 +1,7 @@
+using MicroRabbit.Banca.Data.Contexto;
+using MicroRabbit.Infra.IoC;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +11,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Codigo mio
+builder.Services.AddDbContext<BancaDbConext>(opciones => 
+{
+    opciones.UseSqlServer(builder.Configuration.GetConnectionString("BancaDbConexion"));
+});
+
+// Crear referencias desde API hacia Infra.IoC y los demas
+ContenedorDependencias.RegistrarServicios(builder.Services, builder.Configuration);
+
+//
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
